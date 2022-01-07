f26ee70ad824878e5eb091fdb14e72c5939c2ae2
MugeshGuru <mugeshguru29@gmail.com>
2022-01-07 16:56:14 +0530

    Code is ready

---
+++ src/org/test/SampleOne.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class SampleOne {
+
+	
+	private void empName() {
+		System.out.println("Employee");
+
+	}
+	
+	private void empId() {
+		System.out.println("Emp Id");
+	}
+}
